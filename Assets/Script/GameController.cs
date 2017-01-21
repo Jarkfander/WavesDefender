c5691811bcd98d38a0589efe7713487b952ecdb7
sanctify <bonnafoux.florian@gmail.com>
2017-01-21 17:08:27 +0100

    GameController random mobs
    Ajout de l'aspect random au niveau du type de mobs qui spawn

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -2,79 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
-	public GameObject _Board;
-	public float targetRotationY;
-	public float speed;
-	public GameObject panelBuilding;
-	private GameObject Flag;
+    public GameObject _Board;
+    public float targetRotationY;
+    public float speed;
+    public GameObject panelBuilding;
+    private GameObject Flag;
 
-	//Game over
-	private bool _isGameOver;
+    //Game over
+    private bool _isGameOver;
 
-	//Gestion des monstres
-	[SerializeField]
-	private GameObject _lightMonster = null;
-	[SerializeField]
-	private GameObject _soundMonster = null;
+    //Gestion des monstres
+    [SerializeField]
+    private GameObject _lightMonster = null;
+    [SerializeField]
+    private GameObject _soundMonster = null;
 
-	//Gestion des phases de jeu (calme ou spawning)
-	private bool _isACalmPhase;
-	[SerializeField]
-	private float _calmPhaseDuration = 5.0f;
+    //Gestion des phases de jeu (calme ou spawning)
+    private bool _isACalmPhase;
+    [SerializeField]
+    private float _calmPhaseDuration = 5.0f;
 
-	//Calibrage du spawn de monstres
-	[SerializeField]
-	private float _mobSpawnIntervalTime = 0.85f;
-	[SerializeField]
-	private float _mobsToSpawnNumber = 20;
+    //Calibrage du spawn de monstres
+    [SerializeField]
+    private float _mobSpawnIntervalTime = 0.85f;
+    [SerializeField]
+    private float _mobsToSpawnNumber = 20;
 
-	// Use this for initialization
-	void Start () {
-		_isACalmPhase = true;
-		_isGameOver = false;
-		StartCoroutine(SpawnWaves());
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Mathf.Abs(targetRotationY - _Board.transform.rotation.eulerAngles.y)>0.01 ) {
-			_Board.transform.rotation = Quaternion.Lerp (_Board.transform.rotation, Quaternion.Euler (0.0f, targetRotationY, 0.0f), Time.deltaTime*speed);
-		}
+    // Use this for initialization
+    void Start()
+    {
+        _isACalmPhase = true;
+        _isGameOver = false;
+        StartCoroutine(SpawnWaves());
+    }
 
-		if(Input.GetMouseButtonDown(0)){
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			if(Physics.Raycast (ray, out hit))
-			{
-				Debug.Log (hit.transform.tag + " " + hit.transform.name);
-				if(hit.transform.CompareTag("Flag"))
-				{
-					panelBuilding.SetActive (true);
-					Flag = hit.transform.gameObject;
-				}
-			}
-		}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(targetRotationY - _Board.transform.rotation.eulerAngles.y) > 0.01)
+        {
+            _Board.transform.rotation = Quaternion.Lerp(_Board.transform.rotation, Quaternion.Euler(0.0f, targetRotationY, 0.0f), Time.deltaTime * speed);
+        }
 
-		if(Input.GetMouseButtonDown(1)){
-			panelBuilding.SetActive (false);
-		}
-	}
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.transform.tag + " " + hit.transform.name);
+                if (hit.transform.CompareTag("Flag"))
+                {
+                    panelBuilding.SetActive(true);
+                    Flag = hit.transform.gameObject;
+                }
+            }
+        }
 
-	public void BuildBrassOnFlag(){
+        if (Input.GetMouseButtonDown(1))
+        {
+            panelBuilding.SetActive(false);
+        }
+    }
+
+    public void BuildBrassOnFlag()
+    {
         Flag.GetComponent<BuildFlagController>().build(1);
         panelBuilding.SetActive(false);
     }
 
-	public void BuildPrismOnFlag(){
-		Flag.GetComponent<BuildFlagController> ().build (0);
-		panelBuilding.SetActive (false);
-	}
+    public void BuildPrismOnFlag()
+    {
+        Flag.GetComponent<BuildFlagController>().build(0);
+        panelBuilding.SetActive(false);
+    }
 
     IEnumerator SpawnWaves()
     {
-        if (!_isGameOver) {
+        if (!_isGameOver)
+        {
             if (_isACalmPhase)
             {
                 _isACalmPhase = false;
@@ -85,17 +94,32 @@ public class GameController : MonoBehaviour {
             Transform mobSpawn = null;
 
             // a remplacer par un random
-            GameObject monsterToSpawn = _soundMonster;
+            GameObject monsterToSpawn = null;
+
             MobMover spawnedMonsterMover = null;
 
             while (!_isACalmPhase)
             {
+                Element mobElement = (Element)Random.Range(0, 2);
+                if (mobElement.Equals(Element.Light))
+                {
+                    monsterToSpawn = _lightMonster;
+                }
+                else if (mobElement.Equals(Element.Sound))
+                {
+                    monsterToSpawn = _soundMonster;
+                }
                 mobSpawn = RandomMobSpawn();
                 if (mobsSpawnedNumber < _mobsToSpawnNumber && !_isGameOver)
                 {
-                    GameObject spawnedMonster = (GameObject)Instantiate(monsterToSpawn, mobSpawn.transform.position, mobSpawn.transform.rotation);
-                    spawnedMonsterMover = spawnedMonster.GetComponent<MobMover>();
-                    spawnedMonsterMover.InitializeMobSpawn(mobSpawn);
+                    GameObject spawnedMonster = null;
+                    if (monsterToSpawn != null)
+                    {
+                        spawnedMonster = (GameObject)Instantiate(monsterToSpawn, mobSpawn.transform.position, mobSpawn.transform.rotation);
+                        spawnedMonsterMover = spawnedMonster.GetComponent<MobMover>();
+                        spawnedMonsterMover.InitializeMobSpawn(mobSpawn);
+
+                    }
                     mobsSpawnedNumber++;
                     yield return new WaitForSeconds(_mobSpawnIntervalTime);
                 }
@@ -108,26 +132,32 @@ public class GameController : MonoBehaviour {
         }
     }
 
-    private Transform RandomMobSpawn() {
-        return GameObject.Find("MobSpawn" + (Random.Range(0, 8)+1)).transform;
+    private Transform RandomMobSpawn()
+    {
+        return GameObject.Find("MobSpawn" + (Random.Range(0, 8) + 1)).transform;
     }
 
-    public void GameOver() {
+    public void GameOver()
+    {
         _isGameOver = true;
         DestroyAllExistingEnnemies();
     }
 
-    public void DestroyAllExistingEnnemies() {
-        foreach (GameObject monster in GameObject.FindGameObjectsWithTag("Monster")) {
+    public void DestroyAllExistingEnnemies()
+    {
+        foreach (GameObject monster in GameObject.FindGameObjectsWithTag("Monster"))
+        {
             Destroy(monster.gameObject);
         }
     }
 
-	public void RotateBoardClockwise(){
-		targetRotationY = targetRotationY + 90;
-	}
+    public void RotateBoardClockwise()
+    {
+        targetRotationY = targetRotationY + 90;
+    }
 
-	public void RotateBoardAnticlockwise(){
-		targetRotationY = targetRotationY - 90;
-	}
+    public void RotateBoardAnticlockwise()
+    {
+        targetRotationY = targetRotationY - 90;
+    }
 }

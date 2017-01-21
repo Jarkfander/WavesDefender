a16f785d590693549774f0060d91840644644259
Florent Dupont <florent.dupont.it@gmail.com>
2017-01-21 14:22:26 +0100

    Le GROS commit
    ki va décasser

---
+++ Assets/BuildFlagController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildFlagController : MonoBehaviour {
+
+	public GameObject[] Buildings;
+
+	public void build(int buildingNumber){
+		Buildings [buildingNumber].SetActive(true);
+		this.GetComponent<MeshRenderer> ().enabled = false;
+	}
+}
--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -7,6 +7,8 @@ public class GameController : MonoBehaviour {
 	public GameObject _Board;
 	public float targetRotationY;
 	public float speed;
+	public GameObject panelBuilding;
+	private GameObject Flag;
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +20,23 @@ public class GameController : MonoBehaviour {
 		if (Mathf.Abs(targetRotationY - _Board.transform.rotation.eulerAngles.y)>0.01 ) {
 			_Board.transform.rotation = Quaternion.Lerp (_Board.transform.rotation, Quaternion.Euler (0.0f, targetRotationY, 0.0f), Time.deltaTime*speed);
 		}
+
+		if(Input.GetMouseButtonDown(0)){
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast (ray, out hit))
+			{
+				if(hit.transform.CompareTag("Flag"))
+				{
+					panelBuilding.SetActive (true);
+					Flag = hit.transform.gameObject;
+				}
+			}
+		}
+
+		if(Input.GetMouseButtonDown(1)){
+			panelBuilding.SetActive (false);
+		}
 	}
 
 	public void RotateBoardClockwise(){
@@ -26,5 +45,14 @@ public class GameController : MonoBehaviour {
 
 	public void RotateBoardAnticlockwise(){
 		targetRotationY = targetRotationY - 90;
+	}
+
+	public void BuildBrassOnFlag(){
+
+	}
+
+	public void BuildPrismOnFlag(){
+		Flag.GetComponent<BuildFlagController> ().build (0);
+		panelBuilding.SetActive (false);
 	}
 }

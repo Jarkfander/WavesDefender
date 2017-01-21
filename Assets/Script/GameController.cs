a64374e0e33494eb3ede46f368ee76de1833ecc6
Florent Dupont <florent.dupont.it@gmail.com>
2017-01-21 11:19:45 +0100

    LE GONG (+ le gamecontroller qui marche avec la rotation!)

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -6,7 +6,7 @@ public class GameController : MonoBehaviour {
 
 	public GameObject _Board;
 	public float targetRotationY;
-	public bool _rotationIsClockwise;
+	public float speed;
 
 	// Use this for initialization
 	void Start () {
@@ -16,21 +16,15 @@ public class GameController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Mathf.Abs(targetRotationY - _Board.transform.rotation.eulerAngles.y)>0.01 ) {
-			if (_rotationIsClockwise) {
-				_Board.transform.rotation = Quaternion.Euler (0.0f, _Board.transform.rotation.eulerAngles.y + 0.5f, 0.0f);
-			} else {
-				_Board.transform.rotation = Quaternion.Euler (0.0f, _Board.transform.rotation.eulerAngles.y - 0.5f, 0.0f);
-			}
+			_Board.transform.rotation = Quaternion.Lerp (_Board.transform.rotation, Quaternion.Euler (0.0f, targetRotationY, 0.0f), Time.deltaTime*speed);
 		}
 	}
 
 	public void RotateBoardClockwise(){
 		targetRotationY = targetRotationY + 90;
-		_rotationIsClockwise = true;
 	}
 
 	public void RotateBoardAnticlockwise(){
 		targetRotationY = targetRotationY - 90;
-		_rotationIsClockwise = true;
 	}
 }

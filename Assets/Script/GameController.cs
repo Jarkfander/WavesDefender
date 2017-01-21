2b4a89479db2e27862d8424e94645cceb892a379
Florent Dupont <florent.dupont.it@gmail.com>
2017-01-21 10:43:23 +0100

    Cay la mayrde
    Starrloufloaufl

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject _Board;
+	public float targetRotationY;
+	public bool _rotationIsClockwise;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Mathf.Abs(targetRotationY - _Board.transform.rotation.eulerAngles.y)>0.01 ) {
+			if (_rotationIsClockwise) {
+				_Board.transform.rotation = Quaternion.Euler (0.0f, _Board.transform.rotation.eulerAngles.y + 0.5f, 0.0f);
+			} else {
+				_Board.transform.rotation = Quaternion.Euler (0.0f, _Board.transform.rotation.eulerAngles.y - 0.5f, 0.0f);
+			}
+		}
+	}
+
+	public void RotateBoardClockwise(){
+		targetRotationY = targetRotationY + 90;
+		_rotationIsClockwise = true;
+	}
+
+	public void RotateBoardAnticlockwise(){
+		targetRotationY = targetRotationY - 90;
+		_rotationIsClockwise = true;
+	}
+}

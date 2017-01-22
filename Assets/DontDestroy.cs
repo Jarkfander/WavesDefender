5785e57858e1cd35c9e530dc486f414a73c16d91
Florent Dupont <florent.dupont.it@gmail.com>
2017-01-22 16:37:25 +0100

    MUSIC FLO TROP FORT GRACE A DONTDESTROYONLOAD LALALALA

---
+++ Assets/DontDestroy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class DontDestroy : MonoBehaviour {
+	public static bool alreadyExists = false;
+	// Use this for initialization
+	void Awake () {
+		DontDestroyOnLoad (transform.gameObject);
+		SceneManager.sceneLoaded += OnSceneLoaded;
+		if (alreadyExists) {
+			Destroy (this.gameObject);
+		}
+		alreadyExists = true;
+	}
+
+	void OnDestroy(){
+		SceneManager.sceneLoaded -= OnSceneLoaded;
+	}
+
+	void OnSceneLoaded(Scene scene, LoadSceneMode mode){
+		Debug.Log ("scene " + scene.name);
+		if (scene.name == "Main") {
+			alreadyExists = false;
+			Destroy (this.gameObject);
+		}
+
+
+	}
+
+}

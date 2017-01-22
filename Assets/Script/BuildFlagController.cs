d85769b6634c14add1c141c8664e3dd2328f1bac
Florent Dupont <florent.dupont.it@gmail.com>
2017-01-22 14:09:38 +0100

    Buildeffect

--- Assets/Script/BuildFlagController.cs
+++ Assets/Script/BuildFlagController.cs
@@ -6,10 +6,13 @@ public class BuildFlagController : MonoBehaviour {
 
 	public GameObject[] Buildings;
 	public GameObject flagMesh;
+	public ParticleSystem particules;
 
 	public void build(int buildingNumber){
 		Buildings [buildingNumber].SetActive(true);
 		flagMesh.SetActive (false);
 		this.GetComponent<Collider> ().enabled = false;
+		particules.Play ();
+		Destroy (particules.gameObject, 2.0f);
 	}
 }
